f266d5cfcb369f3f6f96859cadf6822456251ef7
mickeyverve <mickeyportern1@gmail.com>
2022-04-30 21:56:12 +0700

    All permutation

--- src/main/scala/AllPerm.scala
+++ src/main/scala/AllPerm.scala
@@ -1,10 +1,18 @@
 object AllPerm extends App {
 
-  def allPerm(in: List[List[Int]): List[List[Int]] = {
-    def perms0(in: List[Int], tmp: List[Int]): List[List[Int]] =
-      if (in.isEmpty) List(tmp)
-      else in.foldLeft(Nil: List[List[Int]])((acc, el) => perms0(in.filter(en => en != el) , el :: tmp) ++ acc)
-    perms0(in, Nil)
-  }
-  print(allPerm(List("A", "B", "C")))
+//  def allPerm(in: List[List[Int]]): List[List[Int]] = {
+//    def perms0(in: List[Int], tmp: List[Int]): List[List[Int]] =
+//      if (in.isEmpty) List(tmp)
+//      else in.foldLeft(Nil: List[List[Int]])((acc, el) => perms0(in.filter(en => en != el) , el :: tmp) ++ acc)
+//    perms0(in, Nil)
+//  }
+def permutations(list: List[Int]): List[List[Int]] = list match {
+  case Nil => Nil
+  case List(x) => List(List(x))
+  case _ => list
+    .flatMap(x =>
+      permutations(list.filterNot(_==x))
+        .map(p => x :: p))
+}
+  print(permutations(List(1,2,3,4)))
 }
--- src/main/scala/Happy.scala
+++ src/main/scala/Happy.scala
@@ -1,6 +1,9 @@
 object Happy extends App {
   // TODO: write these functions!
-  def sumOfDigitsSquared(n: Int): Int = ???
+  def sumOfDigitsSquared(n: Int): Int =
+    {
+      
+    }
   def isHappy(n: Int): Boolean = ???
   def kThHappy(k: Int): Int = ???
  }
