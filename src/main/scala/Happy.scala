74644d60c14b0884a10cf28e6879938f201335e4
mickeyverve <mickeyportern1@gmail.com>
2022-05-04 23:15:08 +0700

    4-5-22 commit pc

--- src/main/scala/Happy.scala
+++ src/main/scala/Happy.scala
@@ -2,7 +2,7 @@ object Happy extends App {
   // TODO: write these functions!
   def sumOfDigitsSquared(n: Int): Int =
     {
-      
+
     }
   def isHappy(n: Int): Boolean = ???
   def kThHappy(k: Int): Int = ???
