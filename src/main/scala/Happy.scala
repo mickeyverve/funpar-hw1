b66b1a2cf00a9f108792ca576c3522df8b86c549
mickeyverve <mickeyportern1@gmail.com>
2022-05-08 11:04:45 +0700

    8-5-2022 commit pc

--- src/main/scala/Happy.scala
+++ src/main/scala/Happy.scala
@@ -2,21 +2,18 @@ object Happy extends App {
   // TODO: write these functions!
   def sumOfDigitsSquared(n: Int): Int = n.toString.foldLeft(0) { (product, num) => product + num.asDigit * num.asDigit}
 
-    def isHappy(n: Int): Boolean = {
-      new Iterator[Int] {
-        val seen = scala.collection.mutable.Set[Int]()
-        var curr = n
-        def next = {
-          val res = curr
-          curr = res.toString.map(_.asDigit).map(n => n * n).sum
-          seen += res
-          res
-        }
-        def hasNext = !seen.contains(curr)
-        }.toList.last == 1
+  def isHappy(n: Int): Boolean = {
+    def sumOfDigitsSquared(n: Int): Int = n.toString.foldLeft(0) { (product, num) => product + num.asDigit * num.asDigit}
+    def loop(seen: Set[Int],x: Int):Boolean = {
+      x match {
+        case 1 => true
+        case _ if seen.contains(x) => false
+        case _ => loop(seen+x,sumOfDigitsSquared(x))
       }
-
+    }
+    loop(Set(),n)
+  }
   def kThHappy(k: Int): Int = ???
 
-  println(isHappy(236))
+  println(isHappy(24))
  }
