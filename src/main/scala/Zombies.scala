a116a982d0e4fcef6b37041ac34a49b1b540f703
mickeyverve <mickeyportern1@gmail.com>
2022-05-10 16:34:32 +0700

    10-5-22 commit laptop

--- src/main/scala/Zombies.scala
+++ src/main/scala/Zombies.scala
@@ -1,3 +1,44 @@
 object Zombies extends App {
-  def countBad(hs: List[Int]): Int = ???
+  def countBad(hs: List[Int]): Int = {
+    def mergeSort(xs: List[Int]): (Int, List[Int]) = {
+
+      def split_list(nums: List[Int], n: Int): (List[Int], List[Int]) = (nums.take(n), nums.drop(n))
+
+      val n = xs.length / 2
+      if (xs.length <= 1) (0, xs)
+      else {
+        def merge(xs: List[Int], ys: List[Int]): (Int, List[Int]) = (xs, ys) match {
+          case (Nil, Nil) => (0, Nil)
+          case (Nil, y :: yt) => {
+            val merged = merge(xs, yt)
+            (merged._1, y :: merged._2)
+          }
+          case (x :: xt, Nil) => {
+            val merged = merge(xt, ys)
+            (merged._1, x :: merged._2)
+          }
+          case (x :: xt, y :: yt) =>
+            if (x > y) {
+              val merged = merge(xt, ys)
+              (merged._1, x :: merged._2)
+            }
+            else {
+              val merged = merge(xs, yt)
+              (merged._1 + xs.length, y :: merged._2)
+            }
+        }
+
+        val (left, right) = split_list(xs, n)
+        val lSort = mergeSort(left)
+        val rSort = mergeSort(right)
+        val merged = merge(lSort._2, rSort._2)
+        (lSort._1 + rSort._1 + merged._1, merged._2)
+
+      }
+    }
+
+    mergeSort(hs)._1
+  }
+
+  println(countBad(List(1, 7, 22, 13, 25, 4, 10, 34, 16, 28, 19, 31)))
 }
